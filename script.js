2ace7b3d490c0ca871e1c3efcf1e077d11f85261
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2021-02-23 16:05:21 +0100

    added shadow buttons

--- script.js
+++ script.js
@@ -30,3 +30,24 @@ $('.image-upload-wrap').bind('dragover', function () {
 $('.image-upload-wrap').bind('dragleave', function () {
   $('.image-upload-wrap').removeClass('image-dropping');
 });
+
+
+/* SHADOWS */
+
+$(function() {                       
+  $("#buttonShadow1").click(function() {  
+    $('.file-upload-image').addClass("shadow1");
+    $('.file-upload-image').removeClass("shadow2");
+    $('.file-upload-image').removeClass("shadow3");
+  });
+  $("#buttonShadow2").click(function() {  
+    $('.file-upload-image').removeClass("shadow1");
+    $('.file-upload-image').addClass("shadow2");
+    $('.file-upload-image').removeClass("shadow3");
+  });
+  $("#buttonShadow3").click(function() {  
+    $('.file-upload-image').removeClass("shadow1");
+    $('.file-upload-image').removeClass("shadow2");
+    $('.file-upload-image').addClass("shadow3");
+  });
+});
